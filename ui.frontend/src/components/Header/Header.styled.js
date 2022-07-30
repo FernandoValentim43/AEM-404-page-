03177ed466e4ca9ccc7cbb681b2b4bc46dd9a17a
fernando <fernandoreal.valentim@gmail.com>
2022-07-30 11:52:37 -0300

    Fix: sizing fix

--- ui.frontend/src/components/Header/Header.styled.js
+++ ui.frontend/src/components/Header/Header.styled.js
@@ -5,15 +5,22 @@ export const HeaderContainer = styled.header`
   display: flex;
   align-items: center;
   justify-content: left;
-  
-  background-color: lightcoral;
+
+ 
 `;
 
 export const HeaderText = styled.h3`
-  font-family: "Inconsolata";
+  font-family: 'Inconsolata', monospace !important;
   text-transform: uppercase;
   color: #333333;
   font-style: normal;
-  font-weight: 700;
-  font-size: 24px;
+  font-weight: 400;
+  font-size: 1.3rem;
+  margin: 0;
+  padding: 0.5rem 3rem;
+
+  @media (max-width: 780px) {
+    font-size: 1rem;
+    padding: 0rem 1rem;
+}
 `;
